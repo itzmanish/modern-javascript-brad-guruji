758cc4a0d6c0215f402376a6d5a4978e0d825f97
Manish <itzmanish108@gmail.com>
2019-01-08 22:05:23 +0530

    working with string

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
-let val = Math.floor(Math.random() * 10 + 1)
+// variable assign
+const firstName = 'Manish'
+const lastName = 'Kumar'
+let val
+// append
+val = 'Manish '
+val += lastName
+
+// length
+val = firstName.length
+
+// concat
+val = firstName.concat(' ', lastName)
+
+// change case
+val = firstName.toUpperCase()
+val = lastName.toLowerCase()
+
+// index of
+val = firstName.indexOf('h')
+val = firstName[2]
+val = firstName.lastIndexOf('i')
+
+// char At
+val = firstName.charAt(firstName.length - 1)
+val = firstName.charAt('2')
+
+//  substring() - getting string from string like firstName[0:3] in python
+val = firstName.substring(0, 4)
+
+// slice()
+val = firstName.slice(0, 3)
+val = firstName.slice(-3) //  print 3 charcter from last printing will be from left to right
+
+// split()
+const tags = 'web developer,programmer,web designer'
+val = tags.split(',')
+
+// replace()
+val = firstName.replace('Man', 'ash')
+
+// print val
+console.log(firstName, lastName)
 console.log(val);
